f9da8649a26cbbd4b589da67fbd50aab1705824f
JD Park <jdpark@Dionysioss-MacBook-Pro.local>
2024-10-09 16:05:38 +0900

    pagination

---
+++ src/app/_components/AbandonmentPagination.tsx
@@ -0,0 +1,39 @@
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/app/_components/ui/pagination";
+import { Dispatch, SetStateAction } from "react";
+
+interface IProps {
+    currentPage: number;
+    setCurrentPage: Dispatch<SetStateAction<number>>
+}
+
+export default function AbandonmentPagination({currentPage, setCurrentPage}: IProps){
+    const page = Array.from({ length: 5 }, (_, i) => 1+i+(Math.floor(currentPage/5))*5);
+    return (
+        <Pagination>
+            <PaginationContent>
+                <PaginationItem>
+                    <PaginationPrevious onClick={() =>  setCurrentPage(currentPage-1)} />
+                </PaginationItem>
+                {
+                 page.map((pageNumber) => {
+                    return (
+                    <PaginationItem key={pageNumber}>
+                        <PaginationLink isActive={pageNumber === currentPage} onClick={() => setCurrentPage(pageNumber)}>{pageNumber}</PaginationLink>
+                      </PaginationItem>
+                    )
+                 })   
+                }
+                <PaginationItem>
+                    <PaginationNext href="#" onClick={() => setCurrentPage((Math.floor(currentPage/5)+1)*5+1)}/>
+                </PaginationItem>
+            </PaginationContent>
+        </Pagination>
+    )
+}
