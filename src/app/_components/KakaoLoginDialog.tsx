248a73abf6522077dae01bfa094d1523297f88cb
jeongdopark <jeongdo.dev@gmail.com>
2024-10-11 13:47:38 +0000

    env redirectURI

--- src/app/_components/KakaoLoginDialog.tsx
+++ src/app/_components/KakaoLoginDialog.tsx
@@ -14,7 +14,7 @@ import KakaoLogo from "@/static/image/kakao.png"
 export function KakaoLoginDialog({ children }: { children: React.ReactElement }) {
   
   const handleKakaoLogin = () => {
-    const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=http://localhost:3000/auth/kakao`;
+    const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`;
     window.location.href = kakaoAuthURL;
   };
 
